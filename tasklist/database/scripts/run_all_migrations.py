03089240394735d10c8c7e614492bac1e88086f4
Bruno Domingues <bruno_signorelli@outlook.com>
2020-11-06 15:42:12 -0300

    Updated python scripts

--- tasklist/database/scripts/run_all_migrations.py
+++ tasklist/database/scripts/run_all_migrations.py
@@ -4,14 +4,14 @@ from utils.utils import run_all_scripts
 
 
 def main():
-    parser = ArgumentParser(description='Run all migration scripts.')
-    parser.add_argument('migrations_dir', help='Directory with the migrations')
-    parser.add_argument('config', help='Service config file')
-    parser.add_argument('secrets', help='Service database admin secrets')
+    parser = ArgumentParser(description="Run all migration scripts.")
+    parser.add_argument("migrations_dir", help="Directory with the migrations")
+    parser.add_argument("config", help="Service config file")
+    parser.add_argument("secrets", help="Service database admin secrets")
 
     args = parser.parse_args()
     run_all_scripts(args.migrations_dir, args.config, args.secrets)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
