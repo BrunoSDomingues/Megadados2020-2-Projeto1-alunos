58e1c2b8cfd183469a2bb6acd924f92ead7f6091
Bruno Domingues <bruno_signorelli@outlook.com>
2020-11-06 15:41:54 -0300

    Updated migration scripts

--- tasklist/database/migrations/create_users.sql
+++ tasklist/database/migrations/create_users.sql
@@ -1,8 +1,5 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    user_uuid BINARY(16) PRIMARY KEY,
+    uuid BINARY(16) PRIMARY KEY,
     name NVARCHAR(1024)
 );
-
-ALTER TABLE tasks ADD user_uuid BINARY(16);
-ALTER TABLE tasks ADD FOREIGN KEY (user_uuid) REFERENCES users(user_uuid);
---
+++ tasklist/database/migrations/join_tables.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tasks ADD user_uuid BINARY(16);
+ALTER TABLE tasks ADD FOREIGN KEY (user_uuid) REFERENCES users(uuid) ON DELETE CASCADE;
